ea1bc9f1478db3fcf2ebbef19c857668c4c935f1
Reine <reinekaiser@gmail.com>
2023-03-18 22:19:29 +0700

    update

--- Divide.cpp
+++ Divide.cpp
@@ -3,7 +3,7 @@ PhanSo Divide(PhanSo A[], int n){
     PhanSo P;
     P.tu = A[0].tu;
     P.mau= A[0].mau;
-    for (int i=0; i<n; i++){
+    for (int i=1; i<n; i++){
         P.tu=P.tu*A[i].mau;
         P.mau=P.mau*A[i].tu;
     }
