ca9ed18f82b62281bdf832f6c28fe2e50940f3a3
Reine <reinekaiser@gmail.com>
2023-03-20 20:53:53 +0700

    ucln rec

--- ucln_rec.cpp
+++ ucln_rec.cpp
@@ -1,4 +1,10 @@
 #include "PhanSo.h"
+int recUcln(PhanSo a[], int i){
+    int m=a[i].tu;
+    int n=a[i].mau;
+    if (m*n==0) return m+n;
+    return uclnRec(n,m%n);
+}
 int uclnRec(int a, int b){
     if (a*b==0) return a+b;
     return uclnRec(b,a%b);
